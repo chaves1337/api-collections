2c9546d10586454f185a9868cd4356d8e1f7f81a
Matheus Sant'anna <matsnt1223@outlook.com>
2021-03-18 14:36:11 -0300

    created test list class

---
+++ src/main/kotlin/one/digitalInovation/collection/TestList.kt
@@ -0,0 +1,38 @@
+package one.digitalInovation.collection
+
+fun main(){
+
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "CLT")
+    val maria = Funcionario("Maria", 4000.0, "PJ")
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach{
+        println(it)
+}
+    println("-----------------------------")
+
+    println(funcionarios.find { it.nome == "Maria" })
+
+    println("-----------------------------")
+
+    funcionarios
+        .groupBy { it.tipo }
+        .forEach{ println(it)}
+
+
+
+}
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipo: String
+) {
+    override fun toString(): String =
+        """
+            Nome:       $nome
+            Salario:    $salario
+        
+        """.trimIndent()
+}
